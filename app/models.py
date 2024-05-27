05f35846b77966a8d421eb1e7995b7194726de68
Diogo Laureano <diogolaureano.sousa@gmail.com>
2024-05-26 21:21:11 -0300

    Atualização do formulário

--- app/models.py
+++ app/models.py
@@ -4,5 +4,13 @@ from django.db import models
 
 class Pedido(models.Model):
     id_pedido = models.AutoField(primary_key=True)
-    pedido = models.IntegerField()
+    data_da_venda = models.IntegerField(None)
     cliente = models.TextField(max_length=255)
+    produto = models.TextField(max_length=255)
+    quantidade = models.TextField(max_length=255)
+    valor_de_venda = models.TextField(max_length=255)
+    taxas = models.TextField(max_length=255)
+    valor_liquido = models.TextField(max_length=255)
+    prazo = models.TextField(max_length=255)
+    canal_de_venda = models.TextField(max_length=255)
+    codigo_de_rastreio = models.TextField(max_length=255)
