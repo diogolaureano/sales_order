29611a84ccabe3ec215d94e25e9740f1146bee0b
Diogo Laureano <diogolaureano.sousa@gmail.com>
2024-05-30 16:37:30 -0300

    Atualização dos campos e botão do formulário

--- app/models.py
+++ app/models.py
@@ -4,13 +4,14 @@ from django.db import models
 
 class Pedido(models.Model):
     id_pedido = models.AutoField(primary_key=True)
-    data_da_venda = models.IntegerField(None)
+    data_da_venda = models.DateField()
     cliente = models.TextField(max_length=255)
     produto = models.TextField(max_length=255)
-    quantidade = models.TextField(max_length=255)
-    valor_de_venda = models.TextField(max_length=255)
-    taxas = models.TextField(max_length=255)
-    valor_liquido = models.TextField(max_length=255)
-    prazo = models.TextField(max_length=255)
+    quantidade = models.IntegerField(max_length=255)
+    valor_de_venda = models.IntegerField(max_length=255)
+    taxas = models.IntegerField(max_length=255)
+    valor_liquido = models.IntegerField(max_length=255)
+    prazo = models.DateField()
     canal_de_venda = models.TextField(max_length=255)
     codigo_de_rastreio = models.TextField(max_length=255)
+
