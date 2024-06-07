c9ba74458dfb5b3748d291fae823d9fd360d4938
shockobinna <davidokorie59@gmail.com>
2024-06-07 09:55:36 -0300

    Revert "Add new tables"

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from .models import Pedido, Produto, Cliente, Movimentacao, Financeiro
 
-# Register the models
-admin.site.register(Pedido)
-admin.site.register(Produto)
-admin.site.register(Cliente)
-admin.site.register(Movimentacao)
-admin.site.register(Financeiro)
-
+# Register your models here.
